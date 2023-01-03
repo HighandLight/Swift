2c1e6ed048f0988abcc09edbea82cdb7dcf5fdbc
Highandlifht <junhyunghl126@gmail.com>
2023-01-03 18:06:35 +0900

    add I-am-Poor APP

---
+++ I am Poor/I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by 박준형 on 2023/01/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
