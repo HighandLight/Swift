46448338b4505f0a39e4395e43eb823590ed57fc
Highandlifht <junhyunghl126@gmail.com>
2023-01-03 17:40:33 +0900

    add I am Rich APP

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by 박준형 on 2022/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
